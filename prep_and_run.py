0d439ebf1f0bd6783ec4176152c1584fc3748ee6
Jeliazko Jeliazkov <jeliazkov@jhu.edu>
2015-06-05 17:40:03 -0400

    camera issue fixed

--- prep_and_run.py
+++ prep_and_run.py
@@ -1,9 +1,10 @@
 from pymol import cmd
 from pymol import util
 import time
+import numpy as np
 import sys
-sys.path.append("/Users/lqtza/Hacks/ocudump/build")
-from src.cython.ocudump import Ocudump
+sys.path.append("/Users/lqtza/Hacks/ocudump/build/src/cython")
+from ocudump import Ocudump
 
 # oculus tracking data refresh rate, in Hz
 trackingRefresh = 60
@@ -36,7 +37,31 @@ cmd.viewport(1920,1080)
 cmd.full_screen('on')
 
 prevrf = [0,0,0]
- 
+
+def set_origin_at_camera():
+    '''
+    places PyMol's origin (center of rotation) at the camera position, allowing for more natural interaction with PyMol via the head rotations in the Rift.
+    Should probably be called every time the camera makes a translational move
+    explanation:
+        what we want: the displacement vector of the origin of cameraspace (which is also the camera's position) relative to the origin of modelspace, in modelspace coordinates.
+                      If we feed this vector into origin(position=x), the origin will be placed appropriately
+        how to get it: cameraspace and modelspace are two simple 3D cartesian spaces. PyMol's origin of rotation is guaranteed to be a cooincident point in the two spaces.
+                       Based on the info in get_view, we can figure out first the rotation matrix (model->camera). Once the rotation has been applied to modelspace (or it's inverse
+                       to cameraspace), the axes of the two spaces will be aligned. The displacement between the two spaces' origins can then be calculated via a simple 
+                       vector difference of the location of the origin in each space (get_view[9:12] for cameraspace, get_view[12:15] for modelspace)
+    '''
+    view = np.array(cmd.get_view())
+    
+    # simple version
+#     rot = view[0:9].reshape((3,3))
+#     camera = view[9:12]
+#     model = view[12:15]
+#     cmd.origin(position=model - camera.dot(rot.T))
+
+    # faster (?) version
+    cmd.origin(position=view[12:15] - view[9:12].dot(view[0:9].reshape((3,3)).T))
+
+set_origin_at_camera()
 while True:
     #Rift support
     o.getPose()
@@ -46,9 +71,9 @@ while True:
     diff = [currf[0]-prevrf[0],currf[1]-prevrf[1],currf[2]-prevrf[2]]
     #diff = currf
 
-    cmd.turn('x',diff[0]*75)
-    cmd.turn('y',diff[1]*75)
-    cmd.turn('z',diff[2]*75)
+    cmd.turn('x',-diff[0]*25)
+    cmd.turn('y',-diff[1]*25)
+    cmd.turn('z',-diff[2]*25)
     
     time.sleep(1/float(trackingRefresh))
 
