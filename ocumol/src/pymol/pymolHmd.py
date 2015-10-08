a0a3136bbaeb180674e19bb8c3514e4a7427866c
Jeliazko Jeliazkov <jeliazkov@jhu.edu>
2015-10-08 11:29:57 -0400

    minor edits and a center of mass function

--- ocumol/src/pymol/pymolHmd.py
+++ ocumol/src/pymol/pymolHmd.py
@@ -17,54 +17,54 @@ from ocumol.src.helper.transformations import euler_matrix
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 class PymolHmd(threading.Thread):
-    poseCoordDict = {'xRot':0, 'yRot':1, 'zRot':2, 
+    poseCoordDict = {'xRot':0, 'yRot':1, 'zRot':2,
                      'x':3, 'y':4, 'z':5}
-    
+
     def __init__(self, debugMode=False, editMolecule=False, naturalRotation=True, pdb=''):
         # set up the Thread stuff
         threading.Thread.__init__(self)
-        
+
         # pre-initialize the ocudumpDebug, in case we want to set animations
         self.ocudumpDebug = OcudumpDebug()
-        
+
         # initialize previousOrientation (pitch, yaw, roll) and previousPostion (x, y, z)
         self.previousOrientation = [0,0,0]
         self.previousPosition = [0,0,0]
-        
+
         # internal flags
         self.fullscreen = False # whether to run in fullscreen mode or not
         self.running = False    # indicates if this instance's run method has been called
-        
+
         # internal options
         self.rotationScaling = 25   # set scaling factor for rotation
         self.trackingRefresh = 30   # oculus tracking data refresh rate, in Hz
         self.translationScaling = 1 # set scaling factor for translation
-        
+
         # user flags
         self.debugMode = debugMode              # should we run in debug mode (ie indifferent to presence of Rift)?
         self.editMolecule = editMolecule        # set pymol editing mode on/off
         self.naturalRotation = naturalRotation  # define how view rotation is updated
-        
+
         # user options
         self.pdb = pdb  # pdb to load, if any
-    
+
     def initAnimateElement(self, poseCoordName, minim, maxim, period):
         self.ocudumpDebug.initAnimateElement(self.poseCoordDict[poseCoordName], minim, maxim, period)
-    
+
     def initCamera(self):
         self.ocudump.getPose()
 
         # this makes the camera surprisingly jumpy at first
         # max, do we need it?
         # self.basePose = self.ocudump.pose
-    
+
     def initOcudump(self):
         if self.debugMode:
             self.ocudump = self.ocudumpDebug
         else:
             self.ocudump = Ocudump()
         return self.ocudump.init()
-    
+
     def initPymol(self):
 
         # set stereo mode
@@ -75,54 +75,56 @@ class PymolHmd(threading.Thread):
         cmd.set('internal_gui', 0)
 
         # set resolution to HD for rift
-        cmd.viewport(1920, 1080);
+        cmd.viewport(1920, 1080)
 
         # full screen?
-        if self.fullscreen: 
+        if self.fullscreen:
             cmd.full_screen('on')
 
         # load pdb
         if len(self.pdb)==4 and self.pdb==re.match('[a-zA-Z0-9]*', self.pdb).group(0):
             cmd.fetch(self.pdb, async=0)
-        
-        cmd.hide('everything', 'all')
-        cmd.show('cartoon', 'all')
-        
+
+        if self.editMolecule == True:
+            cmd.hide('everything', 'all')
+            cmd.show('cartoon', 'all')
+
         # color each chain differently
         if self.editMolecule:
             util.cbc()
-        
+
         # set origin at camera
         if self.naturalRotation:
             self.setOriginAtCamera()
-    
+
         # move slab away to give a comfortable viewing area
         cmd.clip('move', 10)
-        
+
         # jiggle the camera to deal with blank screen error
         cmd.move('z', .0001)
-    
+
     def reinitPymol(self, pdb):
         if len(self.pdb)==4 and self.pdb==re.match('[a-zA-Z0-9]*', self.pdb).group(0):
             cmd.delete(self.pdb)
         if len(pdb)==4 and pdb==re.match('[a-zA-Z0-9]*', pdb).group(0):
             self.pdb = pdb
             cmd.fetch(self.pdb, async=0)
-        
-        cmd.hide('everything', 'all')
-        cmd.show('cartoon', 'all')
-        
+
+        if self.editMolecule:
+            cmd.hide('everything', 'all')
+            cmd.show('cartoon', 'all')
+
         # color each chain differently
         if self.editMolecule:
             util.cbc()
-        
+
         # set origin at camera
         if self.naturalRotation:
             self.setOriginAtCamera()
-        
+
         # move slab away to give a comfortable viewing area
         cmd.clip('move', 10)
-        
+
     def setOriginAtCamera(self):
         view = np.array(cmd.get_view())
 
@@ -137,16 +139,30 @@ class PymolHmd(threading.Thread):
 
     def setOriginAtMolecule(self):
         view = np.array(cmd.get_view())
-        # TODO: replace this with something that sets the origin at the center of mass
-        cmd.origin(position=view[9:12])
-    
+        #cmd.origin(position=view[9:12])
+        cmd.origin(position=getMoleculeCoM())
+
+    def getMoleculeCoM(self):
+        # "stupid" COM func that gets the com of all atoms
+        # maybe move this to helper?
+        atoms = cmd.get_model("all")
+        xyz = [0,0,0]
+        totalMass = 0
+        for atom in atoms.atom:
+            mass = atom.get_mass()
+            xyz[0] += atom.coord[0]*mass
+            xyz[1] += atom.coord[1]*mass
+            xyz[2] += atom.coord[2]*mass
+            totalMass += mass
+        return [coord/totalMass for coord in xyz]
+
     def visualize(self):
         # load pdb into pymol and set up view
         self.initPymol()
-        
+
         # TODO: check if this is necessary
         self.initCamera()
-        
+
         # listen ...
         while True:
             # Rift support
@@ -172,7 +188,7 @@ class PymolHmd(threading.Thread):
                 cmd.move('x', pose[3] - self.previousPosition[0])
                 cmd.move('y', pose[4] - self.previousPosition[1])
                 cmd.move('z', pose[5] - self.previousPosition[2])
-            
+
             # update camera for "natural rotation"
             if self.naturalRotation:
                 self.setOriginAtCamera()
